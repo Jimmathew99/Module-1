e15127c0db0d16efbb522a51e6bf4b22b8aaa2f5
Jim <jimmathew1999@gmail.com>
2024-03-21 17:53:46 +0530

    done

--- bin/03-20-2024/task/bank.dart
+++ bin/03-20-2024/task/bank.dart
@@ -7,7 +7,7 @@ class Account{
     _balance = value;
   }
   debit(double amount) {
-    if (balance >= amount) {
+    if (balance < amount) {
       print("Debit amount exceeded account balance");
     }
     else {
@@ -20,5 +20,5 @@ main(){
   var bank1=Account();
   bank1.balance;
   print(bank1.balance);
-  bank1.debit(2000);
+  bank1.debit(6100);
 }
